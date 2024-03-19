82c8f7465611f182803705e6d9cd2804c9cabfd5
AsyaXiao <yfcnz2005.smirnova@yandex.ru>
2024-03-19 12:08:03 +0300

    2 try

--- mid.cpp
+++ mid.cpp
@@ -3,7 +3,14 @@
 //
 #include <iostream>
 
-int main() {
 
-    std::cout << "Hello";
+int main() {
+    int x1, x2, y1, y2;
+    std::cin >> x1 >> y1 >> x2 >> y2;
+    if (x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)) {
+        std::cout << "YES";
+    } else {
+        std::cout << "NO";
+    }
+    return 0;
 }
